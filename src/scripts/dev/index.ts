3dff0303feeb1a7969ddd6a20db09e870389f08c
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-13 19:49:43 +0800

    Update entrypoint

--- src/scripts/dev/index.ts
+++ src/scripts/dev/index.ts
@@ -1,25 +1,47 @@
-import { createClient, loadConfig } from '@chookscord/lib';
+import { getCompiler, importTs } from '../../compilers';
+import { createClient } from '@chookscord/lib';
 import { createEventContext } from './utils';
 import { createManagers } from './managers';
-import { register } from 'ts-node';
+import { loadConfig } from './load-config';
 
-register({
-  transpileOnly: true,
-  compilerOptions: {
-    module: 'CommonJS',
-    target: 'ES2021',
-  },
-});
+async function createWatchCompiler() {
+  const compiler = await getCompiler();
+
+  if (compiler.lang === 'js') {
+    return {
+      lang: compiler.lang,
+      compiler: compiler.createCompiler(),
+    };
+  }
+
+  const ts = await importTs();
+  return {
+    lang: compiler.lang,
+    compiler: compiler.createCompiler({
+      module: ts.ModuleKind.CommonJS,
+      moduleResolution: ts.ModuleResolutionKind.NodeJs,
+      target: ts.ScriptTarget.ES2021,
+      outDir: '.chooks',
+      allowJs: true,
+      esModuleInterop: true,
+    }),
+  };
+}
 
 export async function init(): Promise<void> {
-  const config = await loadConfig();
-  if (!config) return;
+  const { lang, compiler } = await createWatchCompiler();
+  const config = await loadConfig(lang, compiler);
+
+  if (!config) {
+    return;
+  }
 
   const client = createClient(config);
   const eventCtx = createEventContext(client.self, config);
   const managers = createManagers(eventCtx);
 
   // Need to login first before registering slash commands
+  await managers.loadEvents();
   await client.login();
-  await managers.load();
+  await managers.loadCommands();
 }
