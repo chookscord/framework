d5ae62f211c0dc1b98ce6f9dd6bb77eae6657d94
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-13 21:51:56 +0800

    Extract file loader

--- src/scripts/dev/loaders/commands.ts
+++ src/scripts/dev/loaders/commands.ts
@@ -5,43 +5,14 @@ import {
   uncachedImport,
 } from '@chookscord/lib';
 import type { WatchCompiler } from '../../../compilers';
-import fs from 'fs/promises';
 import { join } from 'path';
-
-async function opendir(path: string) {
-  try {
-    return await fs.opendir(path);
-  } catch {
-    return null;
-  }
-}
+import { loadFiles } from './_load-files';
 
 export async function loadCommands(
   store: CommandStore,
   commandsDir: string,
   watchCompiler: WatchCompiler,
 ): Promise<void> {
-  // @todo(Choooks22): Extract this from library?
-  async function *loadFiles(dirname: string): AsyncGenerator<string> {
-    const path = join(process.cwd(), dirname);
-    const files = await opendir(path);
-
-    if (!files) {
-      return;
-    }
-
-    for await (const file of files) {
-      if (file.isDirectory()) {
-        const dirpath = join(dirname, file.name);
-        yield* loadFiles(dirpath);
-        continue;
-      }
-
-      const filepath = join(dirname, file.name);
-      yield filepath;
-    }
-  }
-
   for await (const filePath of loadFiles(commandsDir)) {
     watchCompiler.register(filePath, async outPath => {
       const commandPath = join(process.cwd(), outPath);
