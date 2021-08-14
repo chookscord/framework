a649035b9ad7c833abe812c80a2deb049e4d96ad
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-14 17:33:44 +0800

    Simplify command store
    - Moved from functional to classes
    - Simplified to handle either command or text commands, and not both

--- packages/lib/src/cache/stores/_command.ts
+++ packages/lib/src/cache/stores/_command.ts
@@ -1,138 +1,46 @@
 /* eslint-disable complexity */
 import { Command, TextCommand } from '../../types';
+import { isTextCommand } from '../../utils/command-guard';
 
-export interface CommandStore {
-  get: <Type extends 'slash' | 'text'>(
-    type: Type,
-    name: string
-  ) => (
-    Type extends 'slash' ? Command | null :
-    Type extends 'text' ? TextCommand | null :
-    never
-  );
-  set: (command: Command | TextCommand) => void;
-  getAll: <Type extends 'slash' | 'text'>(
-    type: Type
-  ) => IterableIterator<(
-    Type extends 'slash' ? Command :
-    Type extends 'text' ? TextCommand :
-    never
-  )>;
-  remove: (
-    type: 'slash' | 'text',
-    commandName: string
-  ) => void;
-  clear: () => void;
-}
+export class CommandStore<T extends Command | TextCommand> {
+  private _store = new Map<string, T>();
 
-export function createCommandStore(): CommandStore {
-  console.debug('[Command Store]: Command Store created');
-  const slashStore = new Map<string, Command>();
-  const textStore = new Map<string, TextCommand>();
+  public set(commandName: string, command: T): void {
+    this._store.set(commandName.toLowerCase(), command);
+  }
 
-  // Interface and logic is correct, but return type can't be inferred
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const get: CommandStore['get'] = (type, name): any => {
-    switch (type) {
-      case 'slash': return slashStore.get(name) ?? null;
-      case 'text': return textStore.get(name) ?? null;
-      default: throw new TypeError('type must be either "slash" or "text"!');
-    }
-  };
+  public get(commandName: string): T | null {
+    return this._store.get(commandName.toLowerCase()) ?? null;
+  }
 
-  const setSlashCommand = (store: Map<string, Command>, command: Command) => {
-    const commandName = command.name.toLowerCase();
-    if (store.has(commandName)) {
-      console.warn(`[Command Store]: Command "${command.name}" already exists!`);
-      return;
-    }
-    store.set(commandName, command);
-    console.info(`[Command Store]: Command "${command.name}" has been set!`);
-  };
+  public getAll(): Iterable<T> {
+    return new Set(this._store.values()).values();
+  }
 
-  const isTextCommand = (command: Command | TextCommand): command is TextCommand => {
-    return 'text' in command && command.text;
-  };
+  public toArray(): T[] {
+    return [...this.getAll()];
+  }
 
-  const setTextCommand = (store: Map<string, TextCommand>, command: TextCommand) => {
-    const commandName = command.name.toLowerCase();
-    if (store.has(commandName)) {
-      console.warn(`[Command Store]: Text command "${command.name}" already exists!`);
-      return;
-    }
-    store.set(commandName, command);
-    console.info(`[Command Store]: Text command "${command.name}" has been set!`);
+  public get count(): number {
+    return this.toArray().length;
+  }
 
-    if (!('aliases' in command)) {
-      return;
-    }
+  private _delete(commandName: string): void {
+    this._store.delete(commandName.toLowerCase());
+  }
 
-    if (
-      !Array.isArray(command.aliases) ||
-      command.aliases.some(alias => typeof alias !== 'string')
-    ) {
-      console.warn(`[Command Store]: Command aliases for "${command.name}" must be an array of strings!`);
-      return;
-    }
+  public remove(commandName: string): void {
+    const command = this.get(commandName);
 
-    for (const alias of command.aliases) {
-      const aliasName = alias.toLowerCase();
-      store.set(aliasName, command);
-    }
-
-    console.info(`[Command Store]: ${command.aliases} set for "${command.name}"`);
-  };
-
-  const set: CommandStore['set'] = command => {
-    if (isTextCommand(command)) {
-      setTextCommand(textStore, command);
-    } else {
-      setSlashCommand(slashStore, command);
-    }
-  };
-
-  const remove: CommandStore['remove'] = (type, commandName) => {
-    const command = get(type, commandName);
-    if (!command) {
-      console.warn('[Command Store]: Command not found!');
-      return;
-    }
-
-    const store = type === 'slash'
-      ? slashStore
-      : textStore;
-
-    store.delete(command.name.toLowerCase());
-
-    if (isTextCommand(command) && Array.isArray(command.aliases)) {
-      for (const alias of command.aliases) {
-        const aliasName = alias.toLowerCase();
-        store.delete(aliasName);
+    if (command) {
+      this._delete(command.name);
+      if (isTextCommand(command) && Array.isArray(command.aliases)) {
+        command.aliases.forEach(this._delete, this);
       }
     }
-  };
+  }
 
-  const clear: CommandStore['clear'] = () => {
-    textStore.clear();
-    slashStore.clear();
-    console.info('[Command Store]: Command Store cleared.');
-  };
-
-  // Interface and logic is correct, but return type can't be inferred
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const getAll: CommandStore['getAll'] = (type): any => {
-    switch (type) {
-      case 'slash': return slashStore.values();
-      case 'text': return textStore.values();
-      default: throw new TypeError('type must be either "slash" or "text"!');
-    }
-  };
-
-  return {
-    get,
-    set,
-    clear,
-    remove,
-    getAll,
-  };
+  public clear(): void {
+    this._store.clear();
+  }
 }
