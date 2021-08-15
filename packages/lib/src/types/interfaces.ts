4cad36a15949e4a45a8d21213ef3a135ba9194ea
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-15 20:02:16 +0800

    Update config interface

--- packages/lib/src/types/interfaces.ts
+++ packages/lib/src/types/interfaces.ts
@@ -1,11 +1,12 @@
 import type {
-  Awaited,
   Client,
-  ClientEvents,
   ClientOptions,
   IntentsString,
 } from 'discord.js';
-import type { EventContext } from './contexts';
+import type {
+  MessageHandler,
+  MessageValidator,
+} from '..';
 
 export interface ClientInterface {
   readonly self: Client;
@@ -14,37 +15,97 @@ export interface ClientInterface {
 
 export interface Config {
   /**
-   * Your Discord bot token. **DO NOT SHARE THIS TOKEN TO ANYONE.**
+   * Your application credentials. These are essential to make your bot function.
    *
-   * It's recommended to use env files and access `process.env` instead.
-   *
-   * @see {@link https://discord.com/developers/applications Discord Developer Portal}
-   *
-   * @example
-   * // In a file called ".env", put this line:
-   * // DISCORD_BOT_TOKEN=my-discord-bot-token-here
-   * // Then use `process.env.DISCORD_BOT_TOKEN` instead.
-   * const { defineConfig } = require('@chookscord/framework');
-   *
-   * module.exports = defineConfig({
-   *   token: process.env.DISCORD_BOT_TOKEN,
-   * });
+   * You can get the following data from the {@link https://discord.com/developers/applications Discord Developer Portal}
    */
-  token: string;
+  credentials: {
+    /**
+     * Your Discord bot token. **DO NOT SHARE THIS TOKEN TO ANYONE.**
+     *
+     * It's recommended to use env files and access `process.env` instead.
+     *
+     * @example
+     * // In a file called ".env", put this line:
+     * // DISCORD_BOT_TOKEN=my-discord-bot-token-here
+     * // Then use `process.env.DISCORD_BOT_TOKEN` instead.
+     * const { defineConfig } = require('chookscord');
+     *
+     * module.exports = defineConfig({
+     *   token: process.env.DISCORD_BOT_TOKEN,
+     * });
+     */
+    token: string;
+    /**
+     * Your bot's application ID. This is separate from your bot token.
+     *
+     * This is required to register your slash commands,
+     * since it's separate from your bot client.
+     */
+    applicationId: string;
+  };
+  /** The intents your bot will use. */
   intents: IntentsString[];
-  /** Placeholder store for 3rd party site credentials. */
-  credentials?: Record<string, string>;
-  /** Default prefix for non-slash commands. */
-  prefix?: string;
+  /** The discord server to register your slash commands to while developing. */
+  devServer: string;
   /**
-   * Custom non-slash command message validator.
+   * The default prefix for text commands.
    *
-   * Return `true` to continue with the command.
+   * You will need to add `GUILDS` and `GUILD_MESSAGES`
+   * to your intents in order to recieve guild message events.
    */
-  validateMessage?: (
-    ctx: EventContext,
-    ...args: ClientEvents['messageCreate']
-  ) => Awaited<boolean>;
+  prefix?: string;
+  /** Custom handlers for text commands. */
+  message?: {
+    /**
+     * A message validator that should determine
+     * whether the message is a valid command or not.
+     *
+     * @returns {boolean|Promise<boolean>} Whether to continue or not.
+     *
+     * @example
+     * validate(ctx, message) {
+     *   return (
+     *     !message.author.bot && // We should ignore bot messages
+     *     !message.webhookId &&  // We should also ignore webhooks
+     *     // The message should be a command if it starts with our prefix
+     *     message.content.startsWith(ctx.config.prefix)
+     *   );
+     * }
+     */
+    validate?: MessageValidator;
+    /**
+     * A message handler that should resolve to an array with two items,
+     * the `command` and the `args`.
+     * If the command doesn't exist, return an empty array.
+     *
+     * You should only change this **if you know what you're doing**,
+     * the default handler should be enough for most cases.
+     *
+     * @example
+     * // Note: this basic example does not resolve subcommands.
+     * handler(ctx, getCommand, message) {
+     *   // Extract the command name from the message,
+     *   // and pass the rest of the content as the command's args
+     *   const [commandName, ...args] = message.content
+     *     .trim()                          // Remove trailing whitespaces
+     *     .slice(ctx.config.prefix.length) // Remove the prefix from the message
+     *     .split(/ +/g);                   // Split the message at every space
+     *
+     *   // Here we get the command from our saved commands
+     *   const command = getCommand(commandName);
+     *
+     *   // If the command does not exist, return an empty array
+     *   if (!command) {
+     *     return [];
+     *   }
+     *
+     *   // Return the command we found and pass the args as well
+     *   return [command, args];
+     * }
+     */
+    handler?: MessageHandler;
+  };
   /** Extra client options. */
   client?: {
     /** Extra config that will be passed to the Discord.js client. */
