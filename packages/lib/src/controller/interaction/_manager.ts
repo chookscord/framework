6c717b466355b1a36c59f05b9c2aec80df2d6896
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-15 18:17:13 +0800

    Update interaction manager

--- packages/lib/src/controller/interaction/_manager.ts
+++ packages/lib/src/controller/interaction/_manager.ts
@@ -1,38 +1,39 @@
-import { CommandStore, EventContext } from '../../';
+import { CommandStore, EventContext, SlashCommand } from '../..';
+import { InteractionCredentials, createInteractionRegister } from './_register';
 import { createInteractionHandler } from './_handler';
-import { createInteractionRegister } from './_register';
 
 export interface InteractionManager {
-  load: () => void;
-  reload: () => void;
+  load: () => Promise<void>;
+  unload: () => Promise<void>;
+}
+
+export interface DevStuff extends InteractionCredentials {
+  guildId: string;
 }
 
 export function createInteractionManager(
+  store: CommandStore<SlashCommand>,
   ctx: EventContext,
-  commandStore: CommandStore,
+  dev?: DevStuff,
 ): InteractionManager {
   console.debug('[Interaction Manager]: Interaction Manager created.');
-  const interactionRegister = createInteractionRegister(ctx);
-  const interactionHandler = createInteractionHandler(ctx, commandStore);
-
-  const load: InteractionManager['load'] = () => {
-    console.info('[Interaction Manager]: Loading commands...');
-    const commands = [...commandStore.getAll('slash')];
-    interactionRegister.set(commands);
-    interactionHandler.register();
-    console.info(`[Interaction Manager]: ${commands.length} commands loaded.`);
-  };
-
-  const reload: InteractionManager['reload'] = () => {
-    console.info('[Interaction Manager]: Reloading...');
-    interactionHandler.unregister();
-    interactionRegister.clear();
-    load();
-    console.info('[Interaction Manager]: Reloaded.');
-  };
+  const handler = createInteractionHandler(store, ctx);
+  const register = dev
+    ? createInteractionRegister(dev, dev.guildId)
+    : null;
 
   return {
-    load,
-    reload,
+    async load() {
+      console.info('[Interaction Manager]: Loading commands...');
+      handler.register();
+      await register?.register(store.toArray());
+      console.info(`[Interaction Manager]: ${store.count} commands loaded.`);
+    },
+    async unload() {
+      console.info('[Interaction Manager]: Unloading commands...');
+      handler.unregister();
+      await register?.unregister();
+      console.info('[Interaction Manager]: Commands unloaded.');
+    },
   };
 }
