edaeecebeeeb8f45c727c418cafd2150f76f27f1
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-15 18:13:15 +0800

    Add basic interaction manager
    - No support for nested commands yet

--- packages/lib/src/controller/interaction/_handler.ts
+++ packages/lib/src/controller/interaction/_handler.ts
@@ -1,34 +1,40 @@
-import type { CommandStore, EventContext } from '../../';
-import type { Interaction } from 'discord.js';
+/* eslint-disable @typescript-eslint/no-throw-literal */
+import { CommandInteraction, Interaction } from 'discord.js';
+import { CommandStore, SlashCommand } from '../..';
+import { EventContext } from '../../types';
+import { isCommandGroup } from '../../utils/command-guard';
 
-interface InteractionHandler {
+export interface InteractionHandler {
   register: () => void;
   unregister: () => void;
 }
 
 export function createInteractionHandler(
+  store: CommandStore<SlashCommand>,
   ctx: EventContext,
-  commandStore: CommandStore,
 ): InteractionHandler {
   console.debug('[Interaction Handler]: Interaction Handler created.');
-  const handler = async (interaction: Interaction) => {
-    console.debug('[Interaction Handler]: Interaction received.');
 
-    if (!interaction.isCommand()) {
-      console.debug('[Interaction Handler]: Interaction not a command.');
-      return;
+  const execute = async (
+    command: SlashCommand,
+    interaction: CommandInteraction,
+  ) => {
+    if (isCommandGroup(command)) {
+      // @todo(Choooks22): Find nested command and execute
+      console.log('group command', interaction);
+    } else {
+      await command.execute({ ...ctx, interaction });
     }
+  };
 
-    const command = commandStore.get('slash', interaction.commandName);
-    if (!command) {
-      console.debug('[Interaction Handler]: No command found.');
-      return;
-    }
-
+  const runCommand = async (
+    command: SlashCommand,
+    interaction: CommandInteraction,
+  ) => {
     try {
       const start = Date.now();
       console.info(`[Interaction Handler]: Executing command "${command.name}"...`);
-      await command.execute({ ...ctx, interaction });
+      await execute(command, interaction);
       const end = Date.now() - start;
       console.info(`[Interaction Handler]: "${command.name}" finished! Command took: ${end}ms`);
     } catch (error) {
@@ -37,18 +43,23 @@ export function createInteractionHandler(
     }
   };
 
-  const register: InteractionHandler['register'] = () => {
-    ctx.client.on('interactionCreate', handler);
-    console.info('[Interaction Handler]: Interaction Handler registered.');
-  };
+  const handler = async (interaction: Interaction) => {
+    if (!interaction.isCommand()) return;
 
-  const unregister: InteractionHandler['unregister'] = () => {
-    ctx.client.removeListener('interactionCreate', handler);
-    console.info('[Interaction Handler]: Interaction Handler unregistered.');
+    const command = store.get(interaction.commandName);
+    if (!command) return;
+
+    await runCommand(command, interaction);
   };
 
   return {
-    register,
-    unregister,
+    register() {
+      ctx.client.on('interactionCreate', handler);
+      console.info('[Interaction Handler]: Interaction Handler registered.');
+    },
+    unregister() {
+      ctx.client.removeListener('interactionCreate', handler);
+      console.info('[Interaction Handler]: Interaction Handler unregistered.');
+    },
   };
 }
