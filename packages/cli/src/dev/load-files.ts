e0d38ef45ed1abe41e8c4c97dd7cca00adc134b4
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-15 23:22:03 +0800

    Update loader to await until completion

--- packages/cli/src/dev/load-files.ts
+++ packages/cli/src/dev/load-files.ts
@@ -12,15 +12,25 @@ export async function importFiles<T>(
   rootPath: string,
   cb: (data: T) => Awaited<unknown>,
 ): Promise<void> {
-  const transpileFile = (filePath: string): void => {
-    watchCompiler.register(filePath, async outPath => {
-      const path = join(process.cwd(), outPath);
-      const file = await tools.uncachedImport<DefaultExport<T>>(path);
-      cb(file.default);
+  const transpileFile = (filePath: string): Promise<void> => {
+    return new Promise(res => {
+      watchCompiler.register(filePath, async outPath => {
+        const path = join(process.cwd(), outPath);
+        const file = await tools.uncachedImport<DefaultExport<T>>(path);
+        res(); // Resolve promise only once file has been loaded
+        cb(file.default);
+      });
     });
   };
 
+  const jobs: Promise<unknown>[] = [];
+
   for await (const path of tools.traverseDir(rootPath)) {
-    transpileFile(path);
+    const job = transpileFile(path);
+    jobs.push(job);
   }
+
+  // This would essentially wait until all files has been
+  // transpiled and loaded before continuing
+  await Promise.all(jobs);
 }
