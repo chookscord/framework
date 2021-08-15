71f515bc98673da8b97dc13bfc4b2596dbd19f8e
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-15 14:24:16 +0800

    Add client-independent slash command register

--- packages/lib/src/controller/interaction/_register.ts
+++ packages/lib/src/controller/interaction/_register.ts
@@ -1,37 +1,40 @@
-import { Client, ClientApplication } from 'discord.js';
-import { Command, EventContext } from '../../types';
+import type { Command } from '../..';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
 
 export interface InteractionRegister {
-  set: (command: Command[]) => void;
-  clear: () => void;
+  register: (commands: Command[]) => Promise<void>;
+  unregister: () => Promise<void>;
+}
+
+export interface InteractionCredentials {
+  token: string;
+  appId: string;
 }
 
 export function createInteractionRegister(
-  ctx: EventContext,
+  credentials: InteractionCredentials,
+  guild?: string,
 ): InteractionRegister {
   console.debug('[Interaction Register]: Interaction Register created.');
-  const application = new ClientApplication(ctx.client, {});
-  ctx.client.application = application;
+  const rest = new REST({ version: '9' }).setToken(credentials.token);
+  const route = guild
+    ? Routes.applicationGuildCommands(credentials.appId, guild)
+    : Routes.applicationCommands(credentials.appId);
 
-  const set: InteractionRegister['set'] = commands => {
-    const setCommands = () => {
-      application.commands.set(commands);
-    };
-
-    if (!ctx.client.isReady()) {
-      setCommands();
-    } else {
-      (ctx.client as Client).once('ready', setCommands);
-    }
-  };
-
-  const clear: InteractionRegister['clear'] = () => {
-    application.commands.set([]);
-    console.info('[Interaction Register]: Interactions cleared.');
+  const setCommands = async (commands: Command[]) => {
+    await rest.put(route, { body: commands });
   };
 
   return {
-    set,
-    clear,
+    async register(commands) {
+      console.debug('[Interaction Register]: Registering commands...');
+      await setCommands(commands);
+      console.debug(`[Interaction Register]: ${commands.length} commands registered.`);
+    },
+    async unregister() {
+      await setCommands([]);
+      console.info('[Interaction Register]: Interactions cleared.');
+    },
   };
 }
