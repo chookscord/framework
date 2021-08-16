edb3fb5a1b66fe845bbcdc9f8a74d561822bf3c3
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-16 16:49:31 +0800

    Update event register
    - Fix old listeners not overwritten when setting new ones
    - Removed clearing all listeners
      - Other event listeners like message handlers aren't under event
      register, so don't do that

--- packages/lib/src/controller/event/_register.ts
+++ packages/lib/src/controller/event/_register.ts
@@ -1,30 +1,44 @@
 import { Event, EventContext } from '../../types';
+import { Awaited } from 'discord.js';
+import { EventStoreListener } from '../../cache';
 
 export interface EventRegister {
-  set: (event: Event) => void;
-  clear: () => void;
+  set: EventStoreListener;
+  remove: (event: Event) => void;
 }
 
 export function createEventRegister(
   ctx: EventContext,
 ): EventRegister {
   console.debug('[Event Register]: Event Register created.');
-  const set: EventRegister['set'] = event => {
-    const frequency = event.once ? 'once' : 'on';
-    ctx.client[frequency](
-      event.name,
-      (...args) => event.execute(ctx, ...args),
-    );
-    console.info(`[Event Register]: Registered event "${event.name}" to client.`);
-  };
-
-  const clear: EventRegister['clear'] = () => {
-    ctx.client.removeAllListeners();
-    console.info('[Event Register]: Cleared client event listeners.');
-  };
 
   return {
-    set,
-    clear,
+    // @Choooks22: Not sure if this works 100%, might cause leaks, idk
+    set(event, oldEvent) {
+      const frequency = event.once ? 'once' : 'on';
+      // Might break idfk, just make sure to never call set
+      // twice on the same event module
+      event.execute = event.execute.bind(event, ctx);
+      if (oldEvent) {
+        ctx.client.removeListener(
+          oldEvent.name,
+          oldEvent.execute,
+        );
+      } else {
+        ctx.client.removeAllListeners(event.name);
+      }
+      ctx.client[frequency](
+        event.name,
+        event.execute as () => Awaited<void>,
+      );
+      console.info(`[Event Register]: Registered event "${event.name}" to client.`);
+    },
+    remove(event) {
+      ctx.client.removeListener(
+        event.name,
+        event.execute,
+      );
+      console.info(`[Event Register]: Removed event "${event.name}" from client.`);
+    },
   };
 }
