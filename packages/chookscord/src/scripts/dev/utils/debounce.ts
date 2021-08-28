434cb30c47e1a8fd94175c3d7646ed8fb1f5634a
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-28 23:51:09 +0800

    Allow passing params to debounced function

--- packages/chookscord/src/scripts/dev/utils/debounce.ts
+++ packages/chookscord/src/scripts/dev/utils/debounce.ts
@@ -1,12 +1,23 @@
+/* eslint-disable @typescript-eslint/no-shadow */
+export function debounce<T extends [...args: unknown[]]>(
+  fn: (...args: T) => unknown,
+  ms: number
+): typeof fn;
 export function debounce<T extends [...args: unknown[]]>(
   fn: (...args: T) => unknown,
   ms: number,
-): (...args: T) => unknown {
+  ...args: T
+): () => unknown;
+export function debounce<T extends [...args: unknown[]]>(
+  fn: (...args: T) => unknown,
+  ms: number,
+  ...args: T
+): typeof fn {
   let timeout: NodeJS.Timeout;
-  return (...args) => {
+  return (...a) => {
     if (timeout) {
       clearTimeout(timeout);
     }
-    timeout = setTimeout(fn, ms, ...args);
+    timeout = setTimeout(fn, ms, ...args.length ? args : a);
   };
 }
