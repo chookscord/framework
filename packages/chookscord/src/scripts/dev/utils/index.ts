d0d803b19309e627aef47e1522407cb6d988ef1c
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-29 00:13:32 +0800

    Reexport new utils

--- packages/chookscord/src/scripts/dev/utils/index.ts
+++ packages/chookscord/src/scripts/dev/utils/index.ts
@@ -1,4 +1,6 @@
+export * from './is-same';
 export * from './append-path';
 export * from './debounce';
 export * from './import-default';
+export * from './register-commands';
 export * from './types';
