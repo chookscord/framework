067aab6d42c8237f255867bfae3bdb3861dc3c6d
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-28 21:21:03 +0800

    Add bodged rate limit checker

--- packages/lib/src/interactions/register.ts
+++ packages/lib/src/interactions/register.ts
@@ -1,7 +1,9 @@
+/* eslint-disable require-atomic-updates, camelcase */
 import * as chooks from '../types';
 import * as prepare from './_prepare';
 import * as routes from './_routes';
 import { createLogger, fetch } from '../utils';
+import { Response } from 'node-fetch';
 
 export interface InteractionRegister {
   register: (commands: chooks.BaseApplicationCommand[]) => Promise<boolean>;
@@ -14,7 +16,26 @@ export interface InteractionRegisterConfig {
   guildId?: string;
 }
 
+export interface RateLimitError {
+  message: string;
+  retry_after: number;
+  global: boolean;
+}
+
 const logger = createLogger('[lib] Interaction Register');
+
+async function getError(response: Response): Promise<RateLimitError | string | null> {
+  try {
+    const error = await response.json();
+    if ('retry_after' in error) {
+      return error;
+    }
+
+    return error.message ?? null;
+  } catch {
+    return null;
+  }
+}
 
 export function createInteractionRegister(
   config: InteractionRegisterConfig,
@@ -23,6 +44,8 @@ export function createInteractionRegister(
   const route = config.guildId
     ? routes.guild(config.applicationId, config.guildId)
     : routes.global(config.applicationId);
+
+  let retryAfter = 0;
 
   return {
     // @Choooks22: This pains me to add this here since it
@@ -39,7 +62,14 @@ export function createInteractionRegister(
       logger.info(`${counter} commands prepared.`);
       return preparedCommands;
     },
+    // eslint-disable-next-line complexity
     async register(commands) {
+      const cooldown = retryAfter - Date.now();
+      if (cooldown > 0) {
+        logger.warn(`You are still being rate limited! Retry after ${cooldown / 1000}s.`);
+        return false;
+      }
+
       logger.info(`Registering ${commands.length} commands...`);
       const response = await fetch(route, {
         method: 'PUT',
@@ -50,18 +80,24 @@ export function createInteractionRegister(
         body: JSON.stringify(commands),
       });
 
+      logger.debug(`Status: ${response.status} ${response.statusText}`);
+      logger.debug(`OK: ${response.ok}`);
       if (response.ok) {
         logger.info(`${commands.length} commands successfully registered.`);
-      } else {
-        try {
-          const data = await response.json();
-          logger.error(data.errors);
-        } finally {
-          logger.error('Failed to register interactions!');
-        }
+        return true;
       }
 
-      return response.ok;
+      logger.error('Failed to register interactions!');
+      if (response.status === 429) {
+        const error = await getError(response) as RateLimitError;
+        logger.error(`${error.message} Retry after: ${error.retry_after}s.`);
+        retryAfter = Date.now() + error.retry_after * 1000;
+      }
+
+      const error = await getError(response);
+      if (error) logger.error(error);
+
+      return false;
     },
   };
 }
