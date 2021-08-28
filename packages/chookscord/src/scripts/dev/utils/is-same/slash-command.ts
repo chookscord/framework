9cceb6d1c5199d35241d516dfa7c7a57d67847e6
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-28 21:28:42 +0800

    Add util to detect relevant changes from old cache

---
+++ packages/chookscord/src/scripts/dev/utils/is-same/slash-command.ts
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion, complexity, array-bracket-newline */
+import { BaseSlashCommand, NonCommandOption } from '@chookscord/lib';
+import { keysAreSame } from './_object';
+
+export function slashCommandOptionChanged(
+  options: NonCommandOption[],
+  oldOptions: NonCommandOption[],
+): boolean {
+  if (options.length !== oldOptions.length) {
+    return true;
+  }
+
+  for (let i = 0, n = options.length; i < n; i++) {
+    const option = options[i];
+    const oldOption = oldOptions[i];
+
+    if (!keysAreSame(option, oldOption, ['name', 'description', 'type', 'required'])) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+export function slashCommandChanged(
+  interaction: BaseSlashCommand,
+  oldInteraction: BaseSlashCommand | null,
+): boolean {
+  if (!oldInteraction) {
+    return true;
+  }
+
+  if (
+    interaction.name !== oldInteraction.name ||
+    interaction.description !== oldInteraction.description
+  ) {
+    return true;
+  }
+
+  if (
+    !interaction.options &&
+    interaction.options !== oldInteraction.options
+  ) {
+    return true;
+  }
+
+  if (
+    !Array.isArray(interaction.options) ||
+    !Array.isArray(oldInteraction.options)
+  ) {
+    return false;
+  }
+
+  return slashCommandOptionChanged(
+    interaction.options,
+    oldInteraction.options,
+  );
+}
