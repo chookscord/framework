144afdc6197f8e8118e8e77e8287d82f250287a2
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-29 02:47:51 +0800

    Update contexts

--- packages/chookscord/src/scripts/dev/modules/_types.ts
+++ packages/chookscord/src/scripts/dev/modules/_types.ts
@@ -1,9 +1,10 @@
 import { EventContext } from '@chookscord/lib';
 
+export type ModuleContext = Omit<EventContext, 'logger'>;
 export interface ModuleConfig {
   input: string;
   output: string;
-  ctx: EventContext;
+  ctx: ModuleContext;
 }
 
-export type ReloadModule = (ctx: EventContext) => unknown;
+export type ReloadModule = (ctx: ModuleContext) => unknown;
--- packages/chookscord/src/types/context.ts
+++ packages/chookscord/src/types/context.ts
@@ -1,9 +1,13 @@
 import type { Config } from './config';
+import type { Consola } from 'consola';
 import type { FetchUtil } from '@chookscord/lib';
 
 declare module '@chookscord/lib' {
-  export interface EventContext {
+  export interface BaseContext {
     fetch: FetchUtil;
+    logger: Consola;
+  }
+  export interface EventContext {
     config: Config;
   }
 }
