0784e6773762f776bfa0b5ae620f2ea7eab82e90
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-30 01:53:23 +0800

    Update file resolving

--- packages/chookscord/src/scripts/dev/index.ts
+++ packages/chookscord/src/scripts/dev/index.ts
@@ -2,13 +2,14 @@
 import 'dotenv/config';
 process.env.NODE_ENV = 'development';
 import * as lib from '@chookscord/lib';
+import * as modules from './modules';
 import * as path from 'path';
 import * as tools from '../../tools';
 import * as utils from '../../utils';
+import type { ModuleContext, ReloadModule } from './modules/_types';
+import { configFiles, createConfigLoader } from './config';
 import { Client } from 'discord.js';
 import type { Config } from '../../types';
-import { ReloadModule } from './modules/_types';
-import { createConfigLoader } from './config';
 
 const logger = lib.createLogger('[cli] Chooks');
 const fetch = lib.fetch;
@@ -28,9 +29,38 @@ function createClient(config: Config): Client {
   return client;
 }
 
+function *loadModules(
+  ctx: ModuleContext,
+  addedModules: string[],
+): Iterable<ReloadModule | null> {
+  logger.info(`Loading ${addedModules.length} modules...`);
+  const endTimer = utils.createTimer();
+  for (const moduleName of addedModules) {
+    logger.info(`Loading "${moduleName}"...`);
+    const loadModule = modules[moduleName as keyof typeof modules];
+    const reloadModule = loadModule.init({
+      ctx: { ...ctx }, // Respread to avoid mutating other modules
+      input: utils.appendPath.fromRoot(moduleName),
+      output: utils.appendPath.fromOut(moduleName),
+    });
+    yield reloadModule;
+  }
+  logger.success(`Loaded ${addedModules.length} modules. Time took: ${endTimer().toLocaleString()}`);
+}
+
 export async function run(): Promise<void> {
   await logVersion();
-  const [configFile, addedModules] = await tools.findFiles();
+  logger.info('Starting...');
+  const endTimer = utils.createTimer();
+
+  logger.trace('Finding files...');
+  const [configFile, addedModules] = await tools.findFiles({
+    path: process.cwd(),
+    configFiles,
+    directories: Object.keys(modules),
+  });
+  logger.debug(`Config file: "${configFile}"`);
+  logger.debug(`Modules loaded: ${addedModules.map(module => `[${module}]`).join(' ')}`);
 
   if (!configFile) {
     logger.fatal(new Error('Could not find a config file!'));
@@ -47,20 +77,23 @@ export async function run(): Promise<void> {
     }
   };
 
+  logger.trace('Creating config loader.');
   const config = await createConfigLoader({
     inputFile: utils.appendPath.fromRoot(configFile),
     outputPath: utils.appendPath.fromOut(),
     onReload: _reload,
   });
 
+  logger.trace('Creating client.');
   client = createClient(config);
+  const ctx = { client, config, fetch };
 
-  for (const loadModule of addedModules) {
-    const reloadModule = loadModule({ client, config, fetch });
+  for (const reloadModule of loadModules(ctx, addedModules)) {
     loadedModules.push(reloadModule);
   }
 
   logger.info('Client logging in...');
   await client.login();
   logger.success('Client successfully logged in!');
+  logger.info(`Startup time: ${endTimer().toLocaleString()}ms`);
 }
