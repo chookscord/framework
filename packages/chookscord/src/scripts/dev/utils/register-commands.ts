1dbe44de73b0279aa350530fd5ffd73fecc11597
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-29 19:41:34 +0800

    Update register

--- packages/chookscord/src/scripts/dev/utils/register-commands.ts
+++ packages/chookscord/src/scripts/dev/utils/register-commands.ts
@@ -2,20 +2,23 @@ import * as lib from '@chookscord/lib';
 
 const logger = lib.createLogger('[cli] Register');
 export async function registerCommands(
-  register: lib.InteractionRegister,
+  register: lib.RegisterInteraction,
   store: lib.CommandStore<lib.BaseSlashCommand>,
 ): Promise<void> {
   logger.info('Preparing commands...');
-  const commands = register.prepare(store.getAll());
+  const commands = lib.prepareCommands(store.getAll());
   logger.success(`Prepared ${commands.length} commands.`);
+
   try {
     logger.info(`Registering ${commands.length} commands...`);
-    const ok = await register.register(commands);
+    const ok = await register(commands);
+
     if (ok) {
       logger.success(`Successfully registered ${commands.length} commands.`);
     } else {
       logger.warn('Could not register commands.');
     }
+
   } catch (error) {
     logger.error('Failed to register commands!');
     logger.error(error);
