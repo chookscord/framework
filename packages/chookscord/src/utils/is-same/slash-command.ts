51f2030f6420e700073b80d843bd7dcfaa5a7a67
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-01 02:45:25 +0800

    Add check for choices

--- packages/chookscord/src/utils/is-same/slash-command.ts
+++ packages/chookscord/src/utils/is-same/slash-command.ts
@@ -1,6 +1,26 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion, complexity, array-bracket-newline */
-import { BaseSlashCommand, NonCommandOption } from '@chookscord/lib';
+import { BaseSlashCommand, CommandChoice, NonCommandOption } from '@chookscord/lib';
 import { keysAreSame } from './_object';
+
+export function choicesChanged(
+  choices: CommandChoice[],
+  oldChoices: CommandChoice[],
+): boolean {
+  if (choices.length !== oldChoices.length) {
+    return true;
+  }
+
+  for (let i = 0, n = choices.length; i < n; i++) {
+    const choice = choices[i];
+    const oldChoice = oldChoices[i];
+
+    if (!keysAreSame(choice, oldChoice, ['name'])) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 export function slashCommandOptionChanged(
   options: NonCommandOption[],
@@ -15,6 +35,21 @@ export function slashCommandOptionChanged(
     const oldOption = oldOptions[i];
 
     if (!keysAreSame(option, oldOption, ['name', 'description', 'type', 'required'])) {
+      return true;
+    }
+
+    const newIsArray = Array.isArray(option.choices);
+    const oldIsArray = Array.isArray(oldOption.choices);
+
+    if (newIsArray !== oldIsArray) {
+      return true;
+    }
+
+    if (!newIsArray && !oldIsArray) {
+      continue;
+    }
+
+    if (choicesChanged(option.choices!, oldOption.choices!)) {
       return true;
     }
   }
