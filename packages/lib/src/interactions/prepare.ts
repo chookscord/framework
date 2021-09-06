2b1be1cd5a2980c346b0d2b85d240a43790794b8
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-06 13:20:15 +0800

    Update prepare commands
    - Removed validation step
    - Conditionally assign properties to remove unncesseary undefineds

--- packages/lib/src/interactions/prepare.ts
+++ packages/lib/src/interactions/prepare.ts
@@ -1,41 +1,44 @@
 import * as lib from '..';
 import { logger } from './_logger';
-import { validateOption } from '../validation/slash-commands/_option';
 
-function prepareOption(option: lib.CommandOption & { choices?: lib.CommandChoice[] }): lib.ApplicationOption {
-  const error = validateOption(option);
-  if (error) {
-    throw new Error(error);
-  }
+function condAppend<T, K extends keyof T>(
+  object: T,
+  key: K,
+  value: T[K] | undefined,
+): T {
+  return value === undefined
+    ? object
+    : { ...object, [key]: value };
+}
 
+function prepareOption(option: lib.CommandOption & { choices?: (lib.CommandChoice)[] }): lib.ApplicationOption {
   const subOptions = (option as lib.SubCommandOption).options;
+  let appOption = {} as lib.ApplicationOption;
 
-  return {
-    name: option.name,
-    description: option.description,
-    type: lib.CommandOptionType[option.type],
-    choices: option.choices,
-    required: option.required,
-    options: subOptions?.length
-      ? subOptions.map(prepareOption)
-      : undefined,
-  };
+  appOption = condAppend(appOption, 'name', option.name);
+  appOption = condAppend(appOption, 'description', option.description);
+  appOption = condAppend(appOption, 'type', lib.CommandOptionType[option.type]);
+  appOption = condAppend(appOption, 'choices', option.choices);
+  appOption = condAppend(appOption, 'required', option.required);
+  appOption = condAppend(appOption, 'options', subOptions?.length
+    ? subOptions.map(prepareOption)
+    : undefined);
+
+  return appOption;
 }
 
 function prepareCommand(command: lib.SlashCommand): lib.ApplicationSlashCommand {
-  const error = lib.validateBaseCommand(command);
-  if (error) {
-    throw new Error(error);
-  }
-
-  return {
+  let appCommand = {
     type: lib.CommandType.CHAT_INPUT,
-    description: command.description,
-    name: command.name,
-    options: command.options?.length
-      ? command.options.map(prepareOption)
-      : undefined,
-  };
+  } as lib.ApplicationSlashCommand;
+
+  appCommand = condAppend(appCommand, 'name', command.name);
+  appCommand = condAppend(appCommand, 'description', command.description);
+  appCommand = condAppend(appCommand, 'options', command.options?.length
+    ? command.options.map(prepareOption)
+    : undefined);
+
+  return appCommand;
 }
 
 export function prepareCommands(
