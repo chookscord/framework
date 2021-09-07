45edd2d05e7ec4c651f7582b489522fb74df1d50
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-07 18:15:24 +0800

    Add parsing invalid structure message

--- packages/lib/src/interactions/_error.ts
+++ packages/lib/src/interactions/_error.ts
@@ -1,19 +1,64 @@
 /* eslint-disable camelcase */
 import type { Response } from 'node-fetch';
 
-export interface RateLimitError {
+interface BaseError {
+  code: number;
   message: string;
+}
+
+export interface RateLimitError extends BaseError {
   retry_after: number;
   global: boolean;
 }
 
+export interface RegisterError extends BaseError {
+  errors: unknown; // Somewhere down there, RegisterErrorMessage exists.
+}
+
+export interface RegisterErrorMessage {
+  code: string;
+  message: string;
+}
+
+export function isRateLimitError(
+  error: BaseError,
+): error is RateLimitError {
+  return 'retry_after' in error;
+}
+
+export function isRegisterError(
+  error: BaseError,
+): error is RegisterError {
+  return 'errors' in error;
+}
+
+// Stupid hack because for some reason Discord returns some nested trash
+function extractErrorMesage(
+  error: RegisterError,
+): RegisterErrorMessage | null {
+  try {
+    let current = Object.values(error.errors as never)[0];
+    while (!Array.isArray(current)) {
+      current = Object.values(current as never)[0];
+    }
+    return current[0];
+  } catch {
+    return null;
+  }
+}
+
+// eslint-disable-next-line complexity
 export async function getError(
   response: Response,
 ): Promise<RateLimitError | string | null> {
   try {
-    const error = await response.json();
-    if ('retry_after' in error) {
+    const error = await response.json() as BaseError;
+    if (isRateLimitError(error)) {
       return error;
+    }
+
+    if (isRegisterError(error)) {
+      return extractErrorMesage(error)?.message ?? null;
     }
 
     return error.message ?? null;
