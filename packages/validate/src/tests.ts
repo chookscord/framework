b53409691d131320887533a5a98866ca289297bd
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-12 00:02:13 +0800

    Add test each function

--- packages/validate/src/tests.ts
+++ packages/validate/src/tests.ts
@@ -69,3 +69,29 @@ export function assert<T>(
     ? test(value) ? null : message
     : test(value) as ValidationError;
 }
+
+export function testEach<T>(
+  validate: (option: T) => ValidationError,
+): (options: T[]) => ValidationError;
+export function testEach<T>(
+  validate: (option: T) => ValidationError,
+  list: T[],
+): ValidationError;
+export function testEach<T>(
+  validate: (option: T) => ValidationError,
+  list?: T[],
+): ValidationError | ((options: T[]) => ValidationError) {
+  if (!list) {
+    return x => testEach(validate, x);
+  }
+
+  const errors = list.map(validate);
+  return errors.some(isType('string'))
+    ? errors.reduce(
+      (messages, message, i) => message
+        ? `${messages}\n  [${i}] ${message}`
+        : messages,
+      'Errors:',
+    )
+    : null;
+}
