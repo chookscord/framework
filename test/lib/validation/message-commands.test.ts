34a519037440e47492b36c7b79ac5bfe31454db4
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-12 23:38:48 +0800

    Remove unused tests

--- test/lib/validation/message-commands.test.ts
+++
@@ -1,9 +0,0 @@
-describe.skip('validating message commands', () => {
-  describe('proper structures', () => {
-    it.todo('accepts base structure');
-  });
-
-  describe('invalid structures', () => {
-    //
-  });
-});
