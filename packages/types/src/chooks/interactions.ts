d13218548cf58cf08a7cd2f09f903ca26b8ecbeb
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-12 12:53:40 +0800

    Fix missing name field

--- packages/types/src/chooks/interactions.ts
+++ packages/types/src/chooks/interactions.ts
@@ -1,9 +1,11 @@
 export interface ChooksUserCommand {
+  name: string;
   type: 'USER';
   execute(ctx: ChooksUserCommandContext): unknown;
 }
 
 export interface ChooksMessageCommand {
+  name: string;
   type: 'MESSAGE';
   execute(ctx: ChooksMessageCommandContext): unknown;
 }
