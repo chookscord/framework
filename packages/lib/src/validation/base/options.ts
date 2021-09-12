631fa74b2e6f8643b5332c6306198be0b02775a9
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-13 01:37:26 +0800

    Add tests for option types with choices

--- packages/lib/src/validation/base/options.ts
+++ packages/lib/src/validation/base/options.ts
@@ -1,14 +1,60 @@
 import type * as types from '@chookscord/types';
 import * as validate from '@chookscord/validate';
 import { optionIsType } from '../checks';
+import { validateChoiceList } from './choices';
+
+// Maybe expose these somewhere, idk.
+const TYPES_WITH_CHOICES: types.ChooksCommandOptionType[] = [
+  'STRING',
+  'INTEGER',
+  'NUMBER',
+];
+
+const TYPES_WITHOUT_CHOICES: types.ChooksCommandOptionType[] = [
+  'BOOLEAN',
+  'CHANNEL',
+  'MENTIONABLE',
+  'ROLE',
+  'SUB_COMMAND',
+  'SUB_COMMAND_GROUP',
+  'USER',
+];
+
+export function validateOptionWithoutChoice(
+  option: types.ChooksCommandOption,
+): validate.ValidationError {
+  return validate.assert(
+    option.choices,
+    validate.isType('undefined'),
+    'Unexpected choices in option.',
+  );
+}
+
+export function validateOptionWithChoices(
+  option: types.ChooksCommandOption,
+): validate.ValidationError {
+  return validate.assert(
+    option.choices ?? [],
+    validateChoiceList,
+  );
+}
 
 export function validateOption(
   option: types.ChooksCommandOption,
 ): validate.ValidationError {
   return validate.assert(
     option,
-    optionIsType(['STRING', 'INTEGER', 'NUMBER']),
-    'Invalid',
+    option => {
+      if (optionIsType(TYPES_WITH_CHOICES, option)) {
+        return validateOptionWithChoices(option);
+      }
+
+      if (optionIsType(TYPES_WITHOUT_CHOICES, option)) {
+        return validateOptionWithoutChoice(option);
+      }
+
+      return 'Invalid option type.';
+    },
   );
 }
 
