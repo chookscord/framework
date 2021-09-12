3c1cc22d25d7ff9ad314b3deb8d8280961bbf6cb
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-12 21:54:39 +0800

    Add separate tests for commands and interactions

--- packages/validate/src/tests/name.ts
+++ packages/validate/src/tests/name.ts
@@ -8,9 +8,18 @@ export function testName(name: string): ValidationError {
   );
 }
 
-export function testCommandName(name: string): ValidationError {
+export function testInteractionName(name: string): ValidationError {
   return (
     assert(name, testName) ??
     assert(name, testRegex(/^[\w-]{1,32}$/u), 'Name does not match the regex pattern!')
+  );
+}
+
+// This only tests uppercase English letters, not sure about other uppercase letters in
+// different locales but its better to get an error rather than falsely blocking.
+export function testCommandName(name: string): ValidationError {
+  return (
+    assert(name, testInteractionName) ??
+    assert(name, testRegex(/[A-Z]/), 'Name must not have uppercase letters!')
   );
 }
