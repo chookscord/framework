d12c968f288a1fbd6719af3a35c0ae84ce1d9d93
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-13 00:55:33 +0800

    Add validation for slash commands

---
+++ packages/lib/src/validation/commands/slash.ts
@@ -0,0 +1,26 @@
+import type * as types from '@chookscord/types';
+import * as validate from '@chookscord/validate';
+import { validateCommand, validateNonCommandOption } from '../base';
+
+export function validateSlashCommand(
+  command: types.ChooksSlashCommand,
+): validate.ValidationError {
+  return validate.assert(
+    command,
+    validateCommand,
+  ) ??
+  validate.assert(
+    command.type,
+    type => validate.isType('undefined', type) || type === 'CHAT_INPUT',
+    'Invalid command type.',
+  ) ??
+  validate.assert(
+    command.execute,
+    validate.isType('function'),
+    'Missing execute handler.',
+  ) ??
+  validate.assert(
+    command.options ?? [],
+    validate.testEach(validateNonCommandOption),
+  );
+}
