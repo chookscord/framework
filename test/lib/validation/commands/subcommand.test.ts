6183370942534b030d439e2e36e33c389e860e4e
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-13 14:21:03 +0800

    Add placeholder for subcommand tests

---
+++ test/lib/validation/commands/subcommand.test.ts
@@ -0,0 +1,12 @@
+describe.skip('validating subcommands', () => {
+  describe('proper structures', () => {
+    it.todo('accepts base options');
+  });
+
+  describe('invalid structures', () => {
+    describe('invalid options', () => {
+      test.todo('non-command options alongside command groups');
+      test.todo('non-command options alongside subcommands');
+    });
+  });
+});
