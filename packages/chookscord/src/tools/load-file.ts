198f4dde552153fc76fca237fe7a9ee14d7272e0
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-13 22:11:40 +0800

    Add file loader

---
+++ packages/chookscord/src/tools/load-file.ts
@@ -0,0 +1,23 @@
+export type LoadResult<T, R> = {
+  ok: true;
+  data: T;
+} | {
+  ok: false;
+  error: R;
+};
+
+export async function loadFile<T, P, R>(
+  importer: (path: P) => T | Promise<T>,
+  path: P,
+  validate?: (value: T) => R,
+): Promise<LoadResult<T, R | Error>> {
+  try {
+    const data = await importer(path);
+    const error = validate?.(data);
+    return error
+      ? { ok: false, error }
+      : { ok: true, data };
+  } catch (error) {
+    return { ok: false, error: error as Error };
+  }
+}
