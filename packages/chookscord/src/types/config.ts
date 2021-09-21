f437a53eb02003be5231ce65ec2bba023cde4267
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-21 17:59:39 +0800

    Remove unused interface

--- packages/chookscord/src/types/config.ts
+++ packages/chookscord/src/types/config.ts
@@ -26,59 +26,6 @@ export interface BotCredentials {
   applicationId: string;
 }
 
-// @todo(Choooks22): Implement text commands
-// eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/no-unused-vars
-interface MessageHelpers {
-  /**
-   * A message validator that should determine
-   * whether the message is a valid command or not.
-   *
-   * @returns {boolean|Promise<boolean>} Whether to continue or not.
-   *
-   * @example
-   * validate(ctx, message) {
-   *   return (
-   *     !message.author.bot && // We should ignore bot messages
-   *     !message.webhookId &&  // We should also ignore webhooks
-   *     // The message should be a command if it starts with our prefix
-   *     message.content.startsWith(ctx.config.prefix)
-   *   );
-   * }
-   */
-  // validate?: MessageValidator;
-  /**
-   * A message handler that should resolve to an array with two items,
-   * the `command` and the `args`.
-   * If the command doesn't exist, return an empty array.
-   *
-   * You should only change this **if you know what you're doing**,
-   * the default handler should be enough for most cases.
-   *
-   * @example
-   * // Note: this basic example does not resolve subcommands.
-   * handler(ctx, getCommand, message) {
-   *   // Extract the command name from the message,
-   *   // and pass the rest of the content as the command's args
-   *   const [commandName, ...args] = message.content
-   *     .trim()                          // Remove trailing whitespaces
-   *     .slice(ctx.config.prefix.length) // Remove the prefix from the message
-   *     .split(/ +/g);                   // Split the message at every space
-   *
-   *   // Here we get the command from our saved commands
-   *   const command = getCommand(commandName);
-   *
-   *   // If the command does not exist, return an empty array
-   *   if (!command) {
-   *     return [];
-   *   }
-   *
-   *   // Return the command we found and pass the args as well
-   *   return [command, args];
-   * }
-   */
-  // handler?: MessageHandler;
-}
-
 export interface Config {
   /**
    * Your application credentials. These are essential to make your bot function.
@@ -90,8 +37,6 @@ export interface Config {
   intents: IntentsString[];
   /** The discord server to register your slash commands to while developing. */
   devServer?: string;
-  /** Custom helpers for text commands. */
-  // message?: MessageHelpers;
   /** Extra client options. */
   client?: {
     /** Extra config that will be passed to the Discord.js client. */
