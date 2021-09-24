1d6fe9d0ef475eb162e21d419dec0c61fb89eea4
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-25 03:31:32 +0800

    Fix spaces not allowed

--- packages/validate/src/tests/name.ts
+++ packages/validate/src/tests/name.ts
@@ -12,7 +12,7 @@ export function testName(name: string): ValidationError {
 export function testInteractionName(name: string): ValidationError {
   return (
     assert(name, testName) ??
-    assert(name, testRegex(/^[\w-]{1,32}$/u), 'Name does not match the regex pattern!')
+    assert(name, testRegex(/^[\w -]{1,32}$/u), 'Name does not match the regex pattern!')
   );
 }
 
