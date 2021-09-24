b3d965ef6254efff96b19d979fe6d3637aa2fe1b
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-24 23:26:08 +0800

    Add message command validation

--- packages/lib/src/validation/base/commands.ts
+++ packages/lib/src/validation/base/commands.ts
@@ -2,11 +2,28 @@ import type * as types from '@chookscord/types';
 import * as validate from '@chookscord/validate';
 
 export function validateInteraction(
-  interaction: Partial<types.ChooksInteraction>,
+  interaction: Partial<types.ChooksInteractionCommand>,
 ): validate.ValidationError {
   return validate.assert(
     interaction.name!,
     validate.testInteractionName,
+  ) ??
+  validate.assert(
+    // @ts-expect-error validation
+    interaction.description,
+    validate.isType('undefined'),
+    'Unexpected description in interaction.',
+  ) ??
+  validate.assert(
+    // @ts-expect-error validation
+    interaction.options,
+    validate.isType('undefined'),
+    'Unexpected options in interaction.',
+  ) ??
+  validate.assert(
+    interaction.execute,
+    validate.isType('function'),
+    'Missing execute handler.',
   );
 }
 
