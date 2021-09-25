133616ba9e013ee6564063a6c65475b350b5d9dc
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-25 14:39:32 +0800

    Add message commands to register

--- packages/chookscord/src/scripts/register/validators.ts
+++ packages/chookscord/src/scripts/register/validators.ts
@@ -3,4 +3,5 @@ import * as lib from '@chookscord/lib';
 export const validators = {
   commands: lib.validateSlashCommand,
   subcommands: lib.validateSubCommand,
+  messages: lib.validateMessageCommand,
 } as const;
