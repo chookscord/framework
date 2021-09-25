899f8b0645b13cacea36c9f5c586ababfc186104
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-25 18:20:42 +0800

    Merge context command validation

--- packages/lib/src/validation/base/commands.ts
+++ packages/lib/src/validation/base/commands.ts
@@ -1,12 +1,17 @@
 import type * as types from '@chookscord/types';
 import * as validate from '@chookscord/validate';
 
-export function validateContext(
+export function validateContextCommand(
   interaction: Partial<types.ChooksContextCommand>,
 ): validate.ValidationError {
   return validate.assert(
     interaction.name!,
     validate.testInteractionName,
+  ) ??
+  validate.assert(
+    interaction.type,
+    type => type === 'MESSAGE' || type === 'USER',
+    'Invalid command type.',
   ) ??
   validate.assert(
     // @ts-expect-error validation
