744ff8c48a5046f116778d013adf001391359c8e
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-25 18:44:06 +0800

    Add option with choices guards

--- packages/lib/src/guards.ts
+++ packages/lib/src/guards.ts
@@ -1,15 +1,44 @@
-import type { ChooksCommand, ChooksContextCommand, ChooksSlashCommand } from '@chookscord/types';
+import type * as types from '@chookscord/types';
 import { isType } from '@chookscord/validate';
 
+// Maybe expose these somewhere, idk.
+const TYPES_WITH_CHOICES: types.ChooksCommandOptionType[] = [
+  'STRING',
+  'INTEGER',
+  'NUMBER',
+];
+
+const TYPES_WITHOUT_CHOICES: types.ChooksCommandOptionType[] = [
+  'BOOLEAN',
+  'CHANNEL',
+  'MENTIONABLE',
+  'ROLE',
+  'SUB_COMMAND',
+  'SUB_COMMAND_GROUP',
+  'USER',
+];
+
 export function isSlashCommand(
-  command: ChooksCommand,
-): command is ChooksSlashCommand {
+  command: types.ChooksCommand,
+): command is types.ChooksSlashCommand {
   return (command.type ?? 'CHAT_INPUT') === 'CHAT_INPUT' && isType('function', command.execute);
 }
 
 export function isContextCommand(
-  command: ChooksCommand,
-): command is ChooksContextCommand {
+  command: types.ChooksCommand,
+): command is types.ChooksContextCommand {
   return (command.type === 'USER' || command.type === 'MESSAGE') &&
     isType('function', command.execute);
+}
+
+export function isOptionWithChoice(
+  option: types.ChooksCommandOption,
+): option is types.ChooksCommandOptionWithChoice {
+  return TYPES_WITH_CHOICES.includes(option.type);
+}
+
+export function isOptionWithoutChoice(
+  option: types.ChooksCommandOption,
+): option is types.ChooksCommandOptionWithoutChoice {
+  return TYPES_WITHOUT_CHOICES.includes(option.type);
 }
