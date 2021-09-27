a7c5a6fb041bff68aeab23727edc8d44f77df59d
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-27 16:35:48 +0800

    Simplify interaction handling

--- packages/chookscord/src/scripts/dev/loaders/listeners.ts
+++ packages/chookscord/src/scripts/dev/loaders/listeners.ts
@@ -1,41 +1,21 @@
 import * as lib from '@chookscord/lib';
-import type { Client, CommandInteraction } from 'discord.js';
-import { createCommandKey, createTimer } from '../../../utils';
-import type { ChooksCommandContext } from '@chookscord/types';
+import type { Client, Interaction } from 'discord.js';
+import { createTimer, resolveInteractionKey } from '../../../utils';
+import type { ChooksContext } from '@chookscord/types';
 import type { CommandModule } from '../../../types';
 import type { Consola } from 'consola';
-
-interface CommandReference {
-  key: string;
-  command: CommandModule;
-}
 
 // Maybe extract
 function getCtx(
   client: Client,
   commandName: string,
-  interaction: CommandInteraction,
-): ChooksCommandContext {
+  interaction: Interaction,
+): ChooksContext {
   return {
     fetch: lib.fetch,
     client,
     interaction,
     logger: lib.createLogger(`[commands] ${commandName}`),
-  };
-}
-
-function getCommandRef(
-  store: lib.Store<CommandModule>,
-  interaction: CommandInteraction,
-): CommandReference | { key: string; command: null } {
-  const key = createCommandKey(
-    interaction.commandName,
-    interaction.options.getSubcommandGroup(false),
-    interaction.options.getSubcommand(false),
-  );
-  return {
-    key,
-    command: store.get(key),
   };
 }
 
@@ -60,42 +40,19 @@ export function attachInteractionListener(
   store: lib.Store<CommandModule>,
   options: Partial<lib.Logger> = {},
 ): void {
-  // @todo(Choooks22): This should be generalized further to make it reusable
-  const handleCommand = (interaction: CommandInteraction) => {
-    const { key, command } = getCommandRef(store, interaction);
-    if (command) {
-      const ctx = getCtx(client, key, interaction);
-      executeCommand(
-        key,
-        // @todo(Choooks22): Bind dependencies to 'this'
-        command.execute.bind(command, ctx),
-        options.logger,
-      );
-    } else {
-      options?.logger?.warn(`Command "${key}" was executed, but no handlers were registered.`);
-    }
-  };
-
   client.on('interactionCreate', interaction => {
-    if (interaction.isCommand()) {
-      handleCommand(interaction);
-    } else if (interaction.isContextMenu()) {
-      // @todo(Choooks22): Extract this to handle command
-      const key = interaction.commandName;
-      const command = store.get(key);
-      if (command) {
-        executeCommand(
-          key,
-          command.execute.bind(command, {
-            client,
-            fetch: lib.fetch,
-            interaction,
-            logger: lib.createLogger(`[commands] ${key}`),
-          }),
-        );
-      } else {
-        options.logger?.warn(`Command "${key}" was execute, but no handlers were registered.`);
-      }
+    const commandName = resolveInteractionKey(interaction);
+    if (!commandName) return;
+
+    const command = store.get(commandName);
+    if (!command) {
+      options.logger?.warn(`Command "${commandName}" was executed, but no handlers were registered.`);
+      return;
     }
+
+    const ctx = getCtx(client, commandName, interaction);
+    const execute = command.execute.bind(command, ctx);
+
+    executeCommand(commandName, execute);
   });
 }
