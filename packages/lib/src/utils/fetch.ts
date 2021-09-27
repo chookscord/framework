f43c6d64873e13df859497928fbe3de8a641b115
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-27 13:27:39 +0800

    Add extra wrapped getters

--- packages/lib/src/utils/fetch.ts
+++ packages/lib/src/utils/fetch.ts
@@ -3,15 +3,29 @@ import _fetch, { RequestInfo, RequestInit, Response } from 'node-fetch';
 type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
 type FetchParams = [url: RequestInfo, init?: RequestInit];
 
-export type WrappedRequest<T = unknown> = PromiseLike<Response> & { json: () => Promise<T> };
+export type WrappedRequest<T = unknown> =
+Pick<Response, 'text' | 'blob' | 'arrayBuffer'> &
+PromiseLike<Response> & {
+  json: () => Promise<T>;
+  body: Promise<NodeJS.ReadableStream>;
+};
+
 export type WrappedFetch = <T = unknown>(...args: FetchParams) => WrappedRequest<T>;
 export type FetchUtil = WrappedFetch & Record<Lowercase<Method>, WrappedFetch>;
 
 function wrapResponse<T>(request: Promise<Response>): WrappedRequest<T> {
+  const execute = async (key: 'json' | 'blob' | 'text' | 'arrayBuffer') => {
+    const response = await request;
+    return response[key]();
+  };
+
   return {
-    async json() {
-      const res = await request;
-      return res.json();
+    json: () => execute('json'),
+    blob: () => execute('blob'),
+    text: () => execute('text'),
+    arrayBuffer: () => execute('arrayBuffer'),
+    get body() {
+      return request.then(res => res.body);
     },
     then(onfulfilled, onrejected) {
       return request
