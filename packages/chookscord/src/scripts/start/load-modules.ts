bc5f4e78d08046536c8167ff3283a012a01015b6
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-27 23:36:13 +0800

    Cleanup prod interaction handler

--- packages/chookscord/src/scripts/start/load-modules.ts
+++ packages/chookscord/src/scripts/start/load-modules.ts
@@ -2,79 +2,27 @@ import * as lib from '@chookscord/lib';
 import type * as types from '@chookscord/types';
 import * as utils from '../../utils';
 import type { ModuleContext, ModuleName } from '../../types';
-import type { CommandInteraction } from 'discord.js';
+import { executeCommand, getCommandCtx } from '../../tools';
+import { Client } from 'discord.js';
 
 export type CommandHandler = Exclude<types.ChooksCommand['execute'], undefined>;
 
 const logger = lib.createLogger('[cli] Modules');
 
 function attachListener(
-  ctx: ModuleContext,
+  client: Client,
   store: lib.Store<CommandHandler>,
 ) {
-  // @todo(Choooks22): Cleanup this mess
-  const executeHandler = async (
-    commandName: string,
-    execute: () => unknown,
-  ) => {
-    try {
-      logger.info(`Executing command "${commandName}"...`);
-      const endTimer = utils.createTimer();
-      await execute();
-      logger.success(`Finished executing command "${commandName}". Time took: ${endTimer().toLocaleString()}ms`);
-    } catch (error) {
-      logger.error(new Error(`Failed to execute command "${commandName}"!`));
-      logger.error(error);
-    }
-  };
+  client.on('interactionCreate', interaction => {
+    const commandName = utils.resolveInteractionKey(interaction);
+    if (!commandName) return;
 
-  const handleCommands = (interaction: CommandInteraction) => {
-    const commandKey = utils.createCommandKey(
-      interaction.commandName,
-      interaction.options.getSubcommandGroup(false),
-      interaction.options.getSubcommand(false),
-    );
-
-    const execute = store.get(commandKey);
-
-    if (!execute) {
-      logger.warn(`Command "${commandKey}" was executed, but no handler was found!`);
-      return;
-    }
-
-    executeHandler(
-      commandKey,
-      async () => {
-        await execute({
-          logger: lib.createLogger(`[commands] ${commandKey}`),
-          client: ctx.client,
-          fetch: ctx.fetch,
-          interaction,
-        });
-      },
-    );
-  };
-
-  ctx.client.on('interactionCreate', interaction => {
-    if (interaction.isCommand()) {
-      handleCommands(interaction);
-    } else if (interaction.isContextMenu()) {
-      const commandName = interaction.commandName;
-      const execute = store.get(commandName);
-
-      if (!execute) {
-        logger.warn(`Command "${commandName}" was executed, but not handler was found!`);
-        return;
-      }
-
-      executeHandler(commandName, async () => {
-        await execute({
-          logger: lib.createLogger(`[commands] ${commandName}`),
-          client: ctx.client,
-          fetch: ctx.fetch,
-          interaction,
-        });
-      });
+    const execute = store.get(commandName);
+    if (execute) {
+      const ctx = getCommandCtx(client, commandName, interaction);
+      executeCommand(commandName, () => execute(ctx), logger);
+    } else {
+      logger.warn(`Command "${commandName}" was executed, but not handler was found!`);
     }
   });
 }
@@ -85,7 +33,7 @@ export function createModuleLoader(
   let commandStore: lib.Store<CommandHandler>;
   const initCommands = () => {
     commandStore = new lib.Store({ name: '[cli] Commands' });
-    attachListener(ctx, commandStore);
+    attachListener(ctx.client, commandStore);
   };
 
   const getCommandStore = () => {
