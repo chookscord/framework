6debe4a8a9e8f0d220de8590e376a17986adb25e
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-10-10 03:10:31 +0800

    Fix errors with circular dependencies

--- packages/chookscord/src/scripts/dev/unload.ts
+++ packages/chookscord/src/scripts/dev/unload.ts
@@ -4,19 +4,27 @@
  * Traverses a module's children and yields the cache id of each
  * children that contains the target module's id to be unloaded.
  */
-function *unloadChildren(targetId: string, mod: NodeJS.Module): Generator<string, boolean> {
+function *unloadChildren(
+  targetId: string,
+  mod: NodeJS.Module,
+  visited: Set<string> = new Set(),
+): Generator<string, boolean> {
   // should signal to delete the parent
   let deleteSignal = false;
 
   for (const child of mod.children) {
     if (!child.id.includes('.chooks')) continue;
 
+    // Prevent erroring on circular dependencies
+    if (visited.has(child.id)) continue;
+    visited.add(child.id);
+
     if (child.id === targetId) {
       yield child.id;
       deleteSignal = true;
     } else // check if any children references the target module instead
     if (child.children.length) {
-      const values = unloadChildren(targetId, child);
+      const values = unloadChildren(targetId, child, visited);
       let current = values.next();
 
       while (!current.done) {
