da3b57e97da25a1a3d3b924b9473725344c848ca
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-11-25 17:06:49 +0800

    Add dedicated sub command extractor

--- packages/chooksie/src/lib/command.ts
+++ packages/chooksie/src/lib/command.ts
@@ -1,10 +1,16 @@
-import type { ChooksCommandContext, ChooksContextCommandContext } from '../types/chooks';
+import type * as types from '../types/chooks';
 import { ChooksLogger, createLogger } from '@chookscord/logger';
 import type { Client, Interaction } from 'discord.js';
+import { createCommandKey } from './utils';
 import { createTimer } from './chrono';
 import { fetch } from '@chookscord/fetch';
 
-export function getCommandCtx<T extends ChooksContextCommandContext | ChooksCommandContext>(
+export interface CommandReference {
+  module: types.ChooksCommand | types.ChooksSubCommandOption;
+  parent: types.ChooksCommand;
+}
+
+export function getCommandCtx<T extends types.ChooksContextCommandContext | types.ChooksCommandContext>(
   client: Client,
   commandName: string,
   interaction: Interaction,
@@ -26,9 +32,36 @@ export async function executeCommand(
     logger?.info(`Executing command "${commandName}"...`);
     const endTimer = createTimer();
     await execute();
-    logger?.success(`Finished executing command "${commandName}". Time took: ${endTimer().toLocaleString()}ms`);
+    logger?.success(`Finished executing command "${commandName}". Time took: ${endTimer()}`);
   } catch (error) {
     logger?.error(`Failed to execute command "${commandName}"!`);
     logger?.error(error);
+  }
+}
+
+// eslint-disable-next-line complexity
+export function *extractSubCommands(
+  command: types.ChooksSlashSubCommand,
+): Generator<[string, CommandReference]> {
+  const makeRef = (key: string, option: types.ChooksSubCommandOption): [string, CommandReference] => {
+    const ref: CommandReference = {
+      parent: command,
+      module: option,
+    };
+    return [key, ref];
+  };
+
+  for (const option of command.options) {
+    if (option.type === 'SUB_COMMAND') {
+      const key = createCommandKey(command.name, option.name);
+      yield makeRef(key, option as types.ChooksSubCommandOption);
+      continue;
+    }
+    if (option.type === 'SUB_COMMAND_GROUP') {
+      for (const subCommand of option.options) {
+        const key = createCommandKey(command.name, option.name, subCommand.name);
+        yield makeRef(key, subCommand);
+      }
+    }
   }
 }
