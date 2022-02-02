bc011006267bebce42b1d1f52a4ed9691cfc33dd
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-02-02 22:34:16 +0800

    Pass key to resolved interaction

--- packages/chooksie/src/internals/resolve.ts
+++ packages/chooksie/src/internals/resolve.ts
@@ -9,29 +9,42 @@ export interface Stores {
   autocomplete: AutocompleteStore
 }
 
+export interface ResolvedHandler {
+  key: string
+  execute: GenericHandler | null
+}
+
 function createKey(...keys: (string | null)[]): string {
   return keys.filter(Boolean).join(':')
 }
 
-function resolveInteraction(stores: Stores, interaction: Interaction): GenericHandler | null {
+function resolveInteraction(stores: Stores, interaction: Interaction): ResolvedHandler | null {
   if (interaction.isCommand()) {
     const group = interaction.options.getSubcommandGroup(false)
     const subcommand = interaction.options.getSubcommand(false)
+
     const key = createKey(interaction.commandName, group, subcommand)
-    return <GenericHandler>stores.command.get(key) ?? null
+    const execute = <GenericHandler>stores.command.get(key) ?? null
+
+    return { key, execute }
   }
 
   if (interaction.isContextMenu()) {
     const key = interaction.commandName
-    return <GenericHandler>stores.command.get(key) ?? null
+    const execute = <GenericHandler>stores.command.get(key) ?? null
+
+    return { key, execute }
   }
 
   if (interaction.isAutocomplete()) {
     const group = interaction.options.getSubcommandGroup(false)
     const subcommand = interaction.options.getSubcommand(false)
     const option = interaction.options.getFocused(true)
+
     const key = createKey(interaction.commandName, group, subcommand, option.name)
-    return <GenericHandler>stores.autocomplete.get(key) ?? null
+    const execute = <GenericHandler>stores.autocomplete.get(key) ?? null
+
+    return { key, execute }
   }
 
   return null
--- packages/chooksie/tests/resolve.test.ts
+++ packages/chooksie/tests/resolve.test.ts
@@ -1,8 +1,9 @@
 import type { AutocompleteInteraction, CommandInteraction } from 'discord.js'
 import { createKey, resolveInteraction } from '../src/internals/resolve'
 
+const execute = { [Symbol('foo')]: 0 }
 class FakeMap extends Map {
-  public get = jest.fn()
+  public get = jest.fn().mockReturnValue(execute)
 }
 
 function fakeInteraction(type: 'command' | 'context' | 'autocomplete', opts: {
@@ -40,8 +41,11 @@ describe('resolving interactions', () => {
     const map = new FakeMap()
     const interaction = fakeInteraction('command', { name: 'foo' })
 
-    resolveInteraction({ command: map } as never, interaction)
-    expect(map.get).toHaveBeenCalledWith('foo')
+    const key = createKey('foo')
+    const res = resolveInteraction(<never>{ command: map }, interaction)
+
+    expect(map.get).toHaveBeenCalledWith(key)
+    expect(res).toStrictEqual({ key, execute })
   })
 
   test('slash subcommand', () => {
@@ -51,8 +55,11 @@ describe('resolving interactions', () => {
       subcommand: 'bar',
     })
 
-    resolveInteraction({ command: map } as never, interaction)
-    expect(map.get).toHaveBeenCalledWith('foo:bar')
+    const key = createKey('foo', 'bar')
+    const res = resolveInteraction(<never>{ command: map }, interaction)
+
+    expect(map.get).toHaveBeenCalledWith(key)
+    expect(res).toStrictEqual({ key, execute })
   })
 
   test('slash subcommand group', () => {
@@ -63,16 +70,22 @@ describe('resolving interactions', () => {
       subcommand: 'bar',
     })
 
-    resolveInteraction({ command: map } as never, interaction)
-    expect(map.get).toHaveBeenCalledWith('foo:baz:bar')
+    const key = createKey('foo', 'baz', 'bar')
+    const res = resolveInteraction(<never>{ command: map }, interaction)
+
+    expect(map.get).toHaveBeenCalledWith(key)
+    expect(res).toStrictEqual({ key, execute })
   })
 
   test('context menu command', () => {
     const map = new FakeMap()
     const interaction = fakeInteraction('context', { name: 'foo' })
 
-    resolveInteraction({ command: map } as never, interaction)
-    expect(map.get).toHaveBeenCalledWith('foo')
+    const key = createKey('foo')
+    const res = resolveInteraction(<never>{ command: map }, interaction)
+
+    expect(map.get).toHaveBeenCalledWith(key)
+    expect(res).toStrictEqual({ key, execute })
   })
 
   test('autocomplete', () => {
@@ -82,8 +95,11 @@ describe('resolving interactions', () => {
       option: 'baz',
     })
 
-    resolveInteraction({ autocomplete: map } as never, interaction)
-    expect(map.get).toHaveBeenCalledWith('foo:baz')
+    const key = createKey('foo', 'baz')
+    const res = resolveInteraction(<never>{ autocomplete: map }, interaction)
+
+    expect(map.get).toHaveBeenCalledWith(key)
+    expect(res).toStrictEqual({ key, execute })
   })
 
   test('nested autocomplete', () => {
@@ -95,7 +111,10 @@ describe('resolving interactions', () => {
       option: 'qux',
     })
 
-    resolveInteraction({ autocomplete: map } as never, interaction)
-    expect(map.get).toHaveBeenCalledWith('foo:bar:baz:qux')
+    const key = createKey('foo', 'bar', 'baz', 'qux')
+    const res = resolveInteraction(<never>{ autocomplete: map }, interaction)
+
+    expect(map.get).toHaveBeenCalledWith(key)
+    expect(res).toStrictEqual({ key, execute })
   })
 })
