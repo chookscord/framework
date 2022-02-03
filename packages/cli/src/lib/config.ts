6ba6e1dfa95f14ab27cd703d9c6de6f386c8ee33
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-02-03 18:39:18 +0800

    Split config file resolving to lib

--- packages/cli/src/lib/config.ts
+++ packages/cli/src/lib/config.ts
@@ -1,5 +1,16 @@
-import type { ChooksConfig, BotCredentials } from 'chooksie'
+import type { BotCredentials, ChooksConfig } from 'chooksie'
+import type { Dirent } from 'fs'
+import { readdir } from 'fs/promises'
 import Joi from 'joi'
+import { join } from 'path'
+import type { FileOptions, SourceMap } from './sourcemap'
+
+const CONFIG_FILES = [
+  'chooks.config.js',
+  'chooks.config.ts',
+  'chooks.config.dev.js',
+  'chooks.config.dev.ts',
+]
 
 const configSchema = Joi.object<ChooksConfig>({
   credentials: Joi.object<BotCredentials>({
@@ -16,4 +27,23 @@ type DeepPartial<T> = {
 
 export function validateConfig(config: DeepPartial<ChooksConfig>): Joi.ValidationError | null {
   return configSchema.validate(config).error ?? null
+}
+
+export async function resolveConfigFile(opts: FileOptions, files?: Dirent[]): Promise<SourceMap> {
+  const rootFiles = files ?? await readdir(opts.root, { withFileTypes: true })
+
+  const configIndex = rootFiles
+    .filter(file => file.isFile())
+    .reduce((i, file) => Math.max(i, CONFIG_FILES.indexOf(file.name)), -1)
+
+  if (configIndex < 0) {
+    throw new Error('Could not resolve config file!')
+  }
+
+  const configFile = CONFIG_FILES[configIndex]
+  return {
+    source: join(opts.root, configFile),
+    target: join(opts.outDir, 'chooks.config.js'),
+    type: 'config',
+  }
 }
--- packages/cli/src/server/resolve-config.ts
+++ packages/cli/src/server/resolve-config.ts
@@ -1,42 +1,13 @@
 import { interopRequireDefault } from '@swc/helpers'
 import type { ChooksConfig } from 'chooksie'
 import type { Awaitable } from 'discord.js'
-import type { Dirent } from 'fs'
-import { readdir } from 'fs/promises'
-import { join } from 'path'
-import type { FileOptions, SourceMap } from '../lib'
-import { compile, validateConfig } from '../lib'
+import type { FileOptions } from '../lib'
+import { compile, resolveConfigFile, validateConfig } from '../lib'
 import type { WatchCompilerOptions } from './compiler'
 
 export interface ConfigResolverOverrides {
   loader?: (path: string) => Awaitable<ChooksConfig>
   validator?: (config: ChooksConfig) => Awaitable<string | Error | null>
-}
-
-const CONFIG_FILES = [
-  'chooks.config.js',
-  'chooks.config.ts',
-  'chooks.config.dev.js',
-  'chooks.config.dev.ts',
-]
-
-export async function resolveConfigFile(opts: FileOptions, files?: Dirent[]): Promise<SourceMap> {
-  const rootFiles = files ?? await readdir(opts.root, { withFileTypes: true })
-
-  const configIndex = rootFiles
-    .filter(file => file.isFile())
-    .reduce((i, file) => Math.max(i, CONFIG_FILES.indexOf(file.name)), -1)
-
-  if (configIndex < 0) {
-    throw new Error('Could not resolve config file!')
-  }
-
-  const configFile = CONFIG_FILES[configIndex]
-  return {
-    source: join(opts.root, configFile),
-    target: join(opts.outDir, 'chooks.config.js'),
-    type: 'config',
-  }
 }
 
 export async function resolveConfig(
