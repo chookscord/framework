fcb43a103b082d2b9015fc4f39b7c9770e31879c
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-02-10 00:45:18 +0800

    Update entrypoint script

--- packages/cli/src/build/build-target.ts
+++ packages/cli/src/build/build-target.ts
@@ -1,12 +1,16 @@
-import { interopRequireDefault } from '@swc/helpers'
 import type { ChooksScript, Event, GenericHandler, MessageCommand, SlashCommand, SlashSubcommand, UserCommand } from 'chooksie'
-import { createClient, loadEvent, loadMessageCommand, loadScript, loadSlashCommand, loadSlashSubcommand, loadUserCommand, resolveInteraction, walk } from 'chooksie/internals'
+import { createClient, loadEvent, loadMessageCommand, loadScript, loadSlashCommand, loadSlashSubcommand, loadUserCommand, onInteractionCreate, timer, walk } from 'chooksie/internals'
 import type { ClientEvents } from 'discord.js'
 import 'dotenv/config'
+import { relative } from 'path'
 import type { SourceDir } from '../lib'
 import config from './chooks.config.js'
 
 const MODULE_NAMES: SourceDir[] = ['commands', 'subcommands', 'messages', 'users', 'events']
+
+function hasScript(mod: Record<string, unknown>): mod is ChooksScript {
+  return typeof mod.chooksOnLoad === 'function'
+}
 
 function getFileType(path: string): SourceDir | 'scripts' {
   const relPath = path.slice(__dirname.length + 1)
@@ -17,77 +21,75 @@ function getFileType(path: string): SourceDir | 'scripts' {
     : 'scripts'
 }
 
-function hasScript(mod: Record<string, unknown>): mod is ChooksScript {
-  return typeof mod.chooksOnLoad === 'function'
-}
 
 async function main() {
+  console.info('Starting bot...')
+  const measure = timer()
+
   const client = createClient(config)
   const login = client.login(config.token)
 
-  const store = new Map<string, GenericHandler>()
-
-  client.on('interactionCreate', async interaction => {
-    const handler = resolveInteraction(store, interaction)
-    if (!handler) return
-
-    if (!handler.execute) {
-      console.warn(`Handler for "${handler.key}" is missing.`)
-      return
-    }
-
-    try {
-      await handler.execute({ client, interaction })
-    } catch (error) {
-      console.error(`Handler for "${handler.key}" threw an error!`)
-      console.error(error)
-    }
-  })
-
   const files = walk(__dirname, { ignore: file => file.path === __filename })
+  const store = new Map<string, GenericHandler>()
+  const listener = onInteractionCreate(store)
 
-  const loadFile = async (path: string) => {
-    const mod = interopRequireDefault(await import(path) as unknown).default
-    const type = getFileType(path)
+  client.on('interactionCreate', listener)
+
+  for await (const file of files) {
+    const relpath = relative(__dirname, file.path)
+    const mod = await import(file.path) as { default: unknown }
+    const type = getFileType(file.path)
+    console.info(`Loading file "${relpath}"...`)
 
     if (type === 'commands') {
-      await loadSlashCommand(store, mod as SlashCommand)
-      return
+      const command = mod.default as SlashCommand
+      await loadSlashCommand(store, command)
+      console.info(`Loaded slash command "${command.name}".`)
+      continue
     }
 
     if (type === 'subcommands') {
-      await loadSlashSubcommand(store, mod as SlashSubcommand)
-      return
+      const command = mod.default as SlashSubcommand
+      await loadSlashSubcommand(store, command)
+      console.info(`Loaded slash subcommand "${command.name}".`)
+      continue
     }
 
     if (type === 'users') {
-      await loadUserCommand(store, mod as UserCommand)
-      return
+      const command = mod.default as UserCommand
+      await loadUserCommand(store, command)
+      console.info(`Loaded user command "${command.name}".`)
+      continue
     }
 
     if (type === 'messages') {
-      await loadMessageCommand(store, mod as MessageCommand)
-      return
+      const command = mod.default as MessageCommand
+      await loadMessageCommand(store, command)
+      console.info(`Loaded message command "${command.name}".`)
+      continue
     }
 
     if (type === 'events') {
-      await loadEvent(client, mod as Event<keyof ClientEvents>)
-      return
+      const event = mod.default as Event<keyof ClientEvents>
+      await loadEvent(client, event)
+      console.info(`Loaded event "${event.name}".`)
+      continue
     }
 
     if (type === 'scripts') {
       const script = mod as Record<string, unknown>
       if (hasScript(script)) {
         await loadScript(client, script)
+        console.info(`Loaded script at ${relpath}`)
       }
     }
   }
 
-  for await (const file of files) {
-    void loadFile(file.path)
-  }
-
   await login
+  const elapsed = measure()
+
+  console.info('Bot started!')
+  console.info(`Time Took: ${elapsed}`)
 }
 
 void main()
