539cf9e22d269d5c56c53c7bc8fe3498c7d7f051
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-02-13 00:55:22 +0800

    Create base guide site

---
+++ docs/guide/.vuepress/config.ts
@@ -0,0 +1,43 @@
+import { defineUserConfig } from 'vuepress-vite'
+
+// eslint-disable-next-line @typescript-eslint/no-shadow
+const link = (text: string, link: string) => ({ text, link })
+
+export default defineUserConfig({
+  lang: 'en-US',
+  title: 'Chooksie',
+  description: 'Guide for the Chooksie Framework',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Home',
+        children: [
+          link('Introduction', '/introduction/'),
+          link('Getting Started', '/getting-started/'),
+          link('Configuration', '/configuration/'),
+        ],
+      },
+      {
+        text: 'Features',
+        children: [
+          link('Using the CLI Tool', '/features/cli-tool/'),
+          link('Slash Commands', '/features/slash-commands/'),
+          link('Slash Subcommands', '/features/slash-subcommands/'),
+          link('User Commands', '/features/user-commands/'),
+          link('Message Commands', '/features/message-commands/'),
+          link('Event Listeners', '/features/event-listeners/'),
+          link('Autocompletes', '/features/autocompletes/'),
+        ],
+      },
+      {
+        text: 'Advanced',
+        children: [
+          link('Setup Method', '/advanced/setup/'),
+          link('External Scripts', '/advanced/scripts/'),
+          link('Database and Servers', '/advanced/database-servers/'),
+          link('Usage with TypeScript', '/advanced/typescript/'),
+        ],
+      },
+    ],
+  },
+})
---
+++ docs/guide/advanced/setup/README.md
@@ -0,0 +1,5 @@
+---
+title: The Setup Method
+---
+
+# The `setup` Method
