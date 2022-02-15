e29709b45d4c72dcc8716bf40bfc8464bf6299dc
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-02-15 20:58:29 +0800

    Add stream opts

--- packages/chooksie/src/internals/logger.ts
+++ packages/chooksie/src/internals/logger.ts
@@ -1,11 +1,11 @@
-import type { LoggerOptions } from 'pino'
+import type { DestinationStream, LoggerOptions } from 'pino'
 import _pino from 'pino'
 import type { Logger } from '../types'
 
 export type LoggerType = 'autocomplete' | 'command' | 'subcommand' | 'user' | 'message' | 'event' | 'script'
 export type LoggerFactory = (type: LoggerType, name: string) => Logger
 
-function createLogger(opts?: LoggerOptions): LoggerFactory {
+function createLogger(opts?: LoggerOptions | DestinationStream): LoggerFactory {
   const logger = _pino(opts)
   return (type, name): Logger => logger.child({ type, name })
 }
