0a343f3a69537e870a70c9c683b4ed578a5551f5
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-03-03 23:02:50 +0800

    Log used chooksie version on command run

--- packages/cli/src/internals.ts
+++ packages/cli/src/internals.ts
@@ -1,5 +1,4 @@
 import resolveLocal from './lib/resolve'
 
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
 const internals = resolveLocal<typeof import('chooksie/internals')>('chooksie/internals')
 export = internals
