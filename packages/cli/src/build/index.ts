5ffd46c7583f791aa6176f04817d953b4d123a04
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2022-06-12 02:05:05 +0800

    fix: inconsistent ignored files
    resolves #82

--- packages/cli/src/build/index.ts
+++ packages/cli/src/build/index.ts
@@ -27,7 +27,8 @@ const toSourceMap = mapSourceFile({
 const ignored = [
   'dist',
   'node_modules',
-  '.chooks',
+  'test',
+  'tests',
 ]
 
 async function validateModule(file: SourceMap) {
@@ -84,11 +85,11 @@ async function build(): Promise<void> {
   const rootFiles = await readdir(root, { withFileTypes: true })
 
   const jobList = rootFiles
-    .filter(file => file.isDirectory() && !ignored.includes(file.name))
+    .filter(file => file.isDirectory() && !(file.name.startsWith('.') || ignored.includes(file.name)))
     .map(async dir => {
       const filePath = join(root, dir.name)
 
-      const files = walk(filePath, { ignore: file => !/\.(m|c)?js|(?<!\.d)\.(m|c)?ts/.test(file.name) })
+      const files = walk(filePath, { ignore: file => !/^\.|\.(m|c)?js|(?<!\.d)\.(m|c)?ts/.test(file.name) })
       const jobs: Promise<unknown>[] = []
 
       for await (const file of files) {
